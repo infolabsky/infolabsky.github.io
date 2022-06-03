b6ebcccbb80e42d985f60deb12a7c8c8ce78f369
momoji123 <rozaan01@gmail.com>
2022-06-03 10:33:45 +0200

    force uppercase names

--- js/resultProcessor.js
+++ js/resultProcessor.js
@@ -16,7 +16,7 @@ function process(studentInfo){
 
 function renderPassed(studentInfo){
 	DOMUtil.setTextContent("intro-text", INTRO_TEXT_PASS);
-	DOMUtil.setTextContent("student-name", studentInfo.name);
+	DOMUtil.setTextContent("student-name", studentInfo.name.toUpperCase());
 	DOMUtil.setTextContent("student-class", studentInfo.class);
 	DOMUtil.setTextContent("intro-text-end", INTRO_TEXT_END_PASS);
 	DOMUtil.setTextContent("student-result", studentInfo.status.toUpperCase());
@@ -26,7 +26,7 @@ function renderPassed(studentInfo){
 
 function renderFailed(studentInfo){
 	DOMUtil.setTextContent("intro-text", INTRO_TEXT_FAIL);
-	DOMUtil.setTextContent("student-name", studentInfo.name);
+	DOMUtil.setTextContent("student-name", studentInfo.name.toUpperCase());
 	DOMUtil.setTextContent("student-class", studentInfo.class);
 	DOMUtil.setTextContent("intro-text-end", INTRO_TEXT_END_FAIL);
 	DOMUtil.setColor("student-result", "red");
@@ -35,7 +35,7 @@ function renderFailed(studentInfo){
 }
 
 function renderUnknown(studentInfo){
-	DOMUtil.setTextContent("student-name", studentInfo.name);
+	DOMUtil.setTextContent("student-name", studentInfo.name.toUpperCase());
 	DOMUtil.setTextContent("student-class", studentInfo.class);
 	DOMUtil.setTextContent("intro-text-end", INTRO_TEXT_END_UNKNOWN);
 	DOMUtil.setTextContent("student-result", standarizeOutput(studentInfo.status.toUpperCase()));
